4befc20304b644a7deba07077c5047f0f2889c20
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2020-08-16 14:55:37 -0300

    readme images

--- public/javascripts/task.js
+++ public/javascripts/task.js
@@ -1,11 +1,12 @@
 const setTagAsDone = async (element, id) => { //passei como parametro o proprio elemento e o id da task
-    // tenho que chamar o backend e, quando ele responder, preciso alterar o elemtno (setar o chekd para true e passar as classes)
+    // tenho que chamar o backend e alterar o elemento (setar o checked para true e passar as classes certas)
     event.preventDefault();
     try {
         let headers = new Headers({ 'Content-Type': 'application/json' }) //precisamos de header para chamar o backend, e depois o tipo de chamada
         let body = JSON.stringify({ task: { done: element.checked } }) //vou olhar a task, e para saber o que preciso passar, vou olhar o elemento e ver se ele está checado ou não e colocar no body
         let response = await fetch(`/tasks/${id}?_method=put`, { headers: headers, body: body, method: 'PUT' }); //passei minha requisicao
         let data = await response.json(); //pego a resposta que veio da requisicao 
+        
         let task = data.task; //peguei da resposta a minha task atualizada
         let parent = element.parentNode; // como tambem quero alterar o elemento pai, preciso pegar ele aqui (é onde estao as infos de classe do bulma)
 
